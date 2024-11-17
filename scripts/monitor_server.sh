a62ccc9c3b7496bf352647a3fae0d390c1379927
ciherrera20 <ciherrera20@icloud.com>
2024-11-17 23:56:18 +0000

    Monitor server displays last output if it exists

--- scripts/monitor_server.sh
+++ scripts/monitor_server.sh
@@ -9,4 +9,11 @@ if [ -f $server_path/server_pid ] && [ -d /proc/$(cat $server_path/server_pid) ]
 	cat > $server_path/stdin       # Allows for giving the server commands
 else
 	echo $1 not running
+	if [ -f $server_path/stdout ]; then
+		echo last output was
+		cat $server_path/stdout
+		echo end of output
+	else
+		echo no server output
+	fi
 fi	
